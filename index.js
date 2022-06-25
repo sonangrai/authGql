d8316368cf9ec2b271b5ad5bc9e4f1621e256a26
sonangrai <shonahangrae@gmail.com>
2022-06-25 10:07:26 +0545

    port renamed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const resolvers = require("./graphql/resolvers");
 
 const pubsub = new PubSub();
 
-const PORT = process.env.port || 4000;
+const PORT = process.env.PORT || 4000;
 
 const server = new ApolloServer({
   typeDefs,
