77cdf721e487e2133c533450ee19862cc1df46e4
sonangrai <shonahangrae@gmail.com>
2022-06-25 09:59:34 +0545

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+const { ApolloServer, PubSub } = require("apollo-server");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+
+const pubsub = new PubSub();
+
+const PORT = process.env.port || 4000;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req, pubsub }),
+});
+
+mongoose
+  .connect(process.env.MONGOURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected");
+    return server.listen({ port: PORT });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
